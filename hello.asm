8c087d5c6911912b565c2dc9006eb5b5a9410e68
Louis Wilson <louis@louiswins.com>
2020-09-19 12:01:20 -0700

    Hello, world

---
+++ hello.asm
@@ -0,0 +1,18 @@
+.nolist
+#include "ti83plus.inc"
+#define ProgStart $9D95
+.list
+.org ProgStart - 2
+.db t2ByteTok, tAsmCmp
+
+	b_call(_ClrLCDFull)
+	ld hl, 0
+	ld (curCol), hl
+	ld hl, msg
+	b_call(_PutS)
+	b_call(_NewLine)
+	ret
+
+msg:
+	.db "Hello world!", 0
+.end
